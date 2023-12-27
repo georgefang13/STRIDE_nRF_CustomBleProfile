fce11e0aa6b2697aff9f1b4eacabae9bba3f165c
Daniel Elliott <daniel.j.elliott@gmail.com>
2023-12-26 22:07:27 -0500

    Remove Output folder from index
    Remove the pca10040/s132/ses/Output folder from the index to no longer
    track files that are chenged there.

--- pca10040/s132/ses/Output/Debug/Obj/ble_app_template_pca10040_s132/utf.d
+++
@@ -1,9 +0,0 @@
-Output/Debug/Obj/ble_app_template_pca10040_s132/utf.o: \
- C:\Users\danie\Documents\Nordic\ Programs\DeviceDownload\nRF5_SDK_17.1.0_ddde560\external\utf_converter\utf.c \
- C:\Users\danie\Documents\Nordic\ Programs\DeviceDownload\nRF5_SDK_17.1.0_ddde560\external\utf_converter\utf.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/stddef.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL_ConfDefaults.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL_Conf.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL_Arm_Conf.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/stdint.h
