408a2c472a9a3b4590bb34d01d5f78952ce4cec0
Daniel Elliott <daniel.j.elliott@gmail.com>
2024-01-02 11:43:58 -0500

    Update notification timer to 2 seconds
    Set the notification timer to 2 seconds.

--- main.c
+++ main.c
@@ -105,7 +105,7 @@
 #define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(30000)                  /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
 #define MAX_CONN_PARAMS_UPDATE_COUNT    3                                       /**< Number of attempts before giving up the connection parameter negotiation. */
 
-#define NOTIFICATION_INTERVAL           APP_TIMER_TICKS(5000)
+#define NOTIFICATION_INTERVAL           APP_TIMER_TICKS(2000)
 
 #define SEC_PARAM_BOND                  1                                       /**< Perform bonding. */
 #define SEC_PARAM_MITM                  0                                       /**< Man In The Middle protection not required. */
